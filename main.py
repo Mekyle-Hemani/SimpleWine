c5ec1e6dd618b9e89166adfb62d8ae7be7932c29
Mekyle Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2024-09-16 15:47:53 -0400

    Created privilege checking and command execution

--- main.py
+++ main.py
@@ -2,22 +2,41 @@ import save
 import colourprint
 import time
 import os
+import sys
+import subprocess
 
 os_type = os.name
 
+def command(command):
+    process = subprocess.run(command, shell=True, capture_output=True, text=True)
+    colourprint.print_colored(f"Output: \n{process.stdout}", colourprint.ORANGE)
+    if process.stderr:
+        colourprint.print_colored(f"Error: \n{process.stderr}")
+        exit()
+
+
+
+def update():
+    command("sudo apt update && sudo apt upgrade")
+
+
 def osname():
-
-
     if (save.load_part("os") == True):
-        print("passed")
+        update()
     else:
         if os_type == 'posix':
             colourprint.print_colored("OS is running Linux", colourprint.GREEN)
-            save.save_data("os", True)
 
+            if os.geteuid() == 0:
+                colourprint.print_colored("Recieved sudo privileges", colourprint.GREEN)
+            else:
+                colourprint.print_colored("Attempting sudo", colourprint.GREEN)
+                subprocess.call(['sudo', 'python3', *sys.argv])
+                sys.exit()
+
+            save.save_data("os", True)
         else:
             colourprint.print_colored("OS is not Linux based, this file will not work", colourprint.RED)
             save.save_data("os", False)
             quit()
-
 osname()
