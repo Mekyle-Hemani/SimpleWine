dbda1c8eaa3d89c35eaf06bb98090c6bf670ba7e
Mekyle Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2024-09-16 15:30:59 -0400

    OS checking/saving

--- colourprint.py
+++ colourprint.py
@@ -1,2 +1,8 @@
+ORANGE = '38;5;208'
+BLUE = '34'
+YELLOW = '33'
+GREEN = '32'
+RED = '31'
+
 def print_colored(text, color_code):
     print(f"\033[{color_code}m{text}\033[0m")
--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 import save
 import colourprint
+import time
+import os
+
+os_type = os.name
+
+def osname():
+
+
+    if (save.load_part("os") == True):
+        print("passed")
+    else:
+        if os_type == 'posix':
+            colourprint.print_colored("OS is running Linux", colourprint.GREEN)
+            save.save_data("os", True)
+
+        else:
+            colourprint.print_colored("OS is not Linux based, this file will not work", colourprint.RED)
+            save.save_data("os", False)
+            quit()
+
+osname()
