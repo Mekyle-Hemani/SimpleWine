02565f866832ad93586530eb64bd75324c1cc19f
Mekyle Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2024-09-16 16:21:10 -0400

    Created Ubuntu installation

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ os_type = os.name
 def command(command):
     process = subprocess.run(command, shell=True)
     if process.stderr:
-        colourprint.print_colored(f"Error: \n{process.stderr}")
-        exit()
+        colourprint.print_colored(f"Error: \n{process.stderr}", colourprint.RED)
+        quit()
 
 
 def osname():
     if (save.load_part("os") == True):
-        command("sudo apt update && sudo apt upgrade")
+        return True
     else:
         if os_type == 'posix':
             colourprint.print_colored("OS is running Linux", colourprint.GREEN)
@@ -29,8 +29,22 @@ def osname():
                 sys.exit()
 
             save.save_data("os", True)
+            command("sudo apt update && sudo apt upgrade")
+            colourprint.print_colored("Updating...", colourprint.ORANGE)
+            command("sudo reboot")
+            return True
         else:
             colourprint.print_colored("OS is not Linux based, this file will not work", colourprint.RED)
             save.save_data("os", False)
             quit()
-osname()
+
+def install_ubuntu_desktop():
+    if save.load_part("ubuntu_desktop") == True:
+        return True
+    else:
+        command("sudo apt install ubuntu-desktop")
+        save.save_data("ubuntu_desktop", True)
+        return True
+
+if osname() == True:
+    install_ubuntu_desktop()
