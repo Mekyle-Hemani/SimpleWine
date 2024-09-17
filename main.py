4c8fd543eedde079efe88bead83447467bb616a7
Mekyle Hemani <153373890+Mekyle-Hemani@users.noreply.github.com>
2024-09-16 20:16:16 -0400

    Set up display for wine

--- main.py
+++ main.py
@@ -52,10 +52,54 @@ def install_ubuntu_desktop():
             colourprint.print_colored("Ubuntu installed successfully", colourprint.GREEN)
         else:
             colourprint.print_colored(f"Error installing Ubuntu: \n{process.stderr.decode()}", colourprint.RED)
+            return False
         return True
+    
+def lightdm():
+    if save.load_part("lightdm") == True:
+        colourprint.print_colored("Lightdm has been previously verified", colourprint.GREEN)
+        return True
+    else:
+        colourprint.print_colored("Installing lightdm...", colourprint.ORANGE)
+        process = subprocess.run("sudo apt install lightdm", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        if process.returncode == 0:
+            save.save_data(True, "lightdm")
+            colourprint.print_colored("Lightdm installed successfully", colourprint.GREEN)
+            #command("sudo reboot")
+        else:
+            colourprint.print_colored(f"Error installing lightdm: \n{process.stderr.decode()}", colourprint.RED)
+            return False
+        return True
+    
+def defualt_display():
+    if save.load_part("defualt_display") == True:
+        colourprint.print_colored("Defualt display has been previously verified", colourprint.GREEN)
+        return True
+    else:
+        colourprint.print_colored("Installing defualt display...", colourprint.ORANGE)
+        process = subprocess.run("sudo systemctl set-default multi-user.target", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        if process.returncode == 0:
+            save.save_data(True, "defualt_display")
+            colourprint.print_colored("Defualt display installed successfully", colourprint.GREEN)
+            command("sudo reboot")
+        else:
+            colourprint.print_colored(f"Error installing defualt display: \n{process.stderr.decode()}", colourprint.RED)
+            return False
+        return True
+
 
 if osname() == True:
     print(save.load_data())
     time.sleep(5)
-    install_ubuntu_desktop()
-    colourprint.print_colored("Ubuntu installed", colourprint.GREEN)
+    if install_ubuntu_desktop() == False:
+        colourprint.print_colored("Failed installation... Fixing dependency", colourprint.YELLOW)
+        time.sleep(2)
+        command("sudo dpkg --configure -a")
+    time.sleep(2)
+    if lightdm() == False:
+        quit()
+    if defualt_display == False:
+        quit()
+
